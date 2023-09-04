a0f55a6e6f251cc0be6d3b08045eaa5947553756
Christine <christinekhekama99@gmail.com>
2023-09-04 15:31:58 +0300

    enrolling students with their course

--- cli.py
+++ cli.py
@@ -1,6 +1,6 @@
 # cli.py
 import click
-from models import Student, Session
+from models import Student, Course, Session
 
 @click.group()
 def cli():
@@ -31,5 +31,59 @@ def list_students():
     else:
         click.echo('No students found.')
 
+@cli.command()
+@click.option('--name', prompt='Course Name', help='Name of the course')
+def add_course(name):
+    """Add a new course"""
+    session = Session()
+    course = Course(name=name)
+    session.add(course)
+    session.commit()
+    session.close()
+    click.echo(f'Course "{name}" added successfully.')
+
+@cli.command()
+def list_courses():
+    """List all courses"""
+    session = Session()
+    courses = session.query(Course).all()
+    session.close()
+    if courses:
+        click.echo("List of Courses:")
+        for course in courses:
+            click.echo(f'Course ID: {course.id}, Name: {course.name}')
+    else:
+        click.echo('No courses found.')
+
+@cli.command()
+@click.option('--student-id', prompt='Student ID', help='Student ID')
+@click.option('--course-id', prompt='Course ID', help='Course ID')
+def enrol_student(student_id, course_id):
+    """Enroll a student in a course"""
+    session = Session()
+
+    # Fetch the student and course within the same session
+    student = session.query(Student).filter_by(student_id=student_id).first()
+    course = session.query(Course).filter_by(id=course_id).first()
+
+    if not student:
+        click.echo(f'Student with ID "{student_id}" not found.')
+        session.close()
+        return
+
+    if not course:
+        click.echo(f'Course with ID "{course_id}" not found.')
+        session.close()
+        return
+
+    student.courses.append(course)
+    session.commit()
+    click.echo(f'Student "{student.name}" with ID "{student.student_id}" enrolled in course "{course.name}" successfully.')
+
+    # Close the session after the operation
+    session.close()
+
 if __name__ == '__main__':
     cli()
+
+
--- models.py
+++ models.py
@@ -1,9 +1,17 @@
 # models.py
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+# Define a many-to-many association table
+student_courses = Table(
+    'student_courses',
+    Base.metadata,
+    Column('student_id', Integer, ForeignKey('students.id')),
+    Column('course_id', Integer, ForeignKey('courses.id'))
+)
 
 class Student(Base):
     __tablename__ = 'students'
@@ -11,8 +19,20 @@ class Student(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     student_id = Column(String, nullable=False, unique=True)
+    
+    # Define a many-to-many relationship with Course
+    courses = relationship('Course', secondary=student_courses, back_populates='students')
 
-# Replace 'sqlite:///student_tracker.db' with your preferred database URI (e.g., PostgreSQL, MySQL).
+class Course(Base):
+    __tablename__ = 'courses'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    
+    # Define a many-to-many relationship with Student
+    students = relationship('Student', secondary=student_courses, back_populates='courses')
+
+# Create the database engine and session
 DATABASE_URI = 'sqlite:///student_tracker.db'
 engine = create_engine(DATABASE_URI)
 Session = sessionmaker(bind=engine)
